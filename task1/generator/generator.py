d4ebbddd22d4193e6d85d7002812513d53b5ae84
Olliemyers4 <olliemyers4@gmail.com>
2024-08-14 21:37:26 +0100

    Added separate test train and validation sets - output included in commit

--- task1/generator/generator.py
+++ task1/generator/generator.py
@@ -5,10 +5,12 @@ from numpy.random import choice,uniform
 from pandas import DataFrame
 
 class Generator:
-    def __init__(self, schemaFile, plantFile, perPlant,decimalPlaces):
+    def __init__(self, schemaFile, plantFile, trainSize,testSize,validSize,decimalPlaces):
         self.schemaFile = schemaFile
         self.plantFile = plantFile
-        self.perPlant = perPlant
+        self.trainSize = trainSize
+        self.testSize = testSize
+        self.validSize = validSize
         self.dp = decimalPlaces
         self.schema = None
         self.plants = None
@@ -33,6 +35,11 @@ class Generator:
             self.ValidateJson(plant, self.schema)
 
     def Generate(self):
+        self.GenerateSet(self.trainSize,"train.csv")
+        self.GenerateSet(self.testSize,"test.csv")
+        self.GenerateSet(self.validSize,"validation.csv")
+
+    def GenerateSet(self,setSize,fileName):
         generatedPlants = []
         for plant in self.plants:
             label = plant["name"]
@@ -42,23 +49,23 @@ class Generator:
             colours = plant["colours"]
             heightmin = plant["heightmin"]
             heightmax = plant["heightmax"]
-            colourList = choice(colours,self.perPlant,replace=True)
-            heights = uniform(float(heightmin),float(heightmax),self.perPlant)
+            colourList = choice(colours,setSize,replace=True)
+            heights = uniform(float(heightmin),float(heightmax),setSize)
             heights = map(lambda x: round(x,self.dp),heights)
             plantList = map(lambda x,y: {"label":label,"climate":climate,"shape":shape,"flowering":flowering,"colour":x,"height":y},colourList,heights)
             generatedPlants.extend(plantList)
         df = DataFrame(generatedPlants)
-        df.to_csv("generatedPlants.csv",index=False)
+        df.to_csv(fileName,index=False)
         
 
 
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 5:
-        print("Usage: python generator.py plantSchema.json plants.json SamplesPerPlant DecimalPlacesOnHeight")
+    if len(args) != 7:
+        print("Usage: python generator.py plantSchema.json plants.json TrainSize TestSize ValidationSize DecimalPlacesOnHeight")
         sys.exit(1)
-    g = Generator(args[1], args[2], int(args[3]), int(args[4]))
+    g = Generator(args[1], args[2], int(args[3]), int(args[4]), int(args[5]), int(args[6]))
     g.Validate()
     g.Generate()
 
