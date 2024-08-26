b19b79315ae021052de0058eff81c7e0179e95c1
Olliemyers4 <olliemyers4@gmail.com>
2024-08-26 12:53:32 +0100

    Basic working solution

--- task3/cartPole.py
+++ task3/cartPole.py
@@ -1,14 +1,65 @@
 import dqn
 import gymnasium as gym
+import torch
+import math
+
+def epsilonDecay(start,end,decayRate,steps):
+    return end + (start - end) * math.exp(-1. * steps / decayRate)
 
 env = gym.make('CartPole-v1',render_mode = "human")
-observation, info = env.reset()
 
-for _ in range(1000):
-    action = env.action_space.sample() # replace this with the dqn
-    observation, reward, terminated, truncated, info = env.step(action)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    if terminated or truncated:
-        observation,info = env.reset()
+policyNet = dqn.DQN(env.observation_space.shape[0], 32, env.action_space.n).to(device)
+targetNet = dqn.DQN(env.observation_space.shape[0], 32, env.action_space.n).to(device)
+targetNet.load_state_dict(policyNet.state_dict())
+
+memory = dqn.ReplayMemory(1000)
+batchSize = 32
+episodes = 1000
+gamma = 0.99 # discount factor
+lr = 0.001
+optimiser = torch.optim.Adam(policyNet.parameters(), lr=lr)
+tau = 0.01 # soft update rate
+params = dqn.Parameters(batchSize=batchSize,gamma=gamma,device=device,optimiser=optimiser,learningRate=lr,tau=tau)
+steps = 0
+
+epsStart = 1
+epsEnd = 0.01
+epsDecay = 1000
+
+
+for _ in range(episodes):
+    observation, info = env.reset()
+    observation = torch.tensor([observation], device=params.device, dtype=torch.float)
+
+    while True:
+        #while we can continue
+        epsilonThreshold = epsilonDecay(epsStart,epsEnd,epsDecay,steps)
+        action = dqn.selectAction(observation=observation,epsilonThreshold=epsilonThreshold,env=env,model=policyNet,params=params)
+        steps += 1
+        actionOutcome, reward, terminated, truncated, info = env.step(action.item())
+
+        reward = torch.tensor([reward], device=params.device)
+        
+        if (terminated):
+            nextObservation = None
+        else:
+            nextObservation = torch.tensor([actionOutcome], device=params.device, dtype=torch.float)
+
+        # Add to the transition memory
+        memory.push(observation, action,nextObservation,reward)
+      
+        observation = nextObservation
+
+        dqn.optimiser(params=params,memory=memory,policyModel=policyNet,targetModel=targetNet)
+
+        targetState = targetNet.state_dict()
+        policyState = policyNet.state_dict()
+        for key in policyState:
+            targetState[key] = policyState[key] * params.tau + targetState[key] * (1.0 - params.tau)
+
+        if (terminated or truncated):
+            break
 
 env.close()
--- task3/dqn.py
+++ task3/dqn.py
@@ -16,22 +16,76 @@ class DQN(torch.nn.Module):
         x = self.fc3(x)
         return x
     
-class Transition(object):
-    def __init__(self, state, action, reward, nextState):
-        self.state = state
-        self.action = action
-        self.reward = reward
-        self.nextState = nextState
+Transition = namedtuple('Transition', ('observation', 'action', 'nextObservation', 'reward'))
     
 class ReplayMemory(object):
     def __init__(self, capacity):
-        self.mem = deque(maxlen=capacity)
+        self.mem = deque([],maxlen=capacity)
     
-    def push(self, transition):
-        self.mem.append(transition)
+    def push(self, *transition):
+        self.mem.append(Transition(*transition))
     
     def sample(self, batchSize):
         return random.sample(self.mem, batchSize)
     
     def __len__(self):
         return len(self.mem)
+    
+class Parameters(object):
+    def __init__(self, batchSize, gamma,device,optimiser,learningRate,tau):
+        self.batchSize = batchSize
+        self.gamma = gamma
+        self.device = device
+        self.optimiser = optimiser
+        self.learningRate = learningRate
+        self.tau = tau
+
+
+def selectAction(observation,epsilonThreshold,env,model,params):
+    # epsilon greedy
+    epsilonRandom = random.random()
+    if epsilonRandom < epsilonThreshold:
+        # greedy
+        return torch.tensor([[env.action_space.sample()]], device=params.device, dtype=torch.long)
+    else:
+        # what the network thinks
+        with torch.no_grad():
+            return model(observation).max(1).indices.view(1, 1)
+
+
+def optimiser(params,memory,policyModel,targetModel):
+    if len(memory) < params.batchSize:
+        return # not enough samples to perform sampling
+    transitions = memory.sample(params.batchSize)
+    batchedSamples = Transition(*zip(*transitions))
+
+    # filter out the final states - boolean mask
+    nonFinalBoolMask = torch.tensor(tuple(map(lambda s: s is not None, batchedSamples.nextObservation)), device=params.device, dtype=torch.bool)
+
+    # filter out the final states
+    nonFinalNextObservations= torch.cat([s for s in batchedSamples.nextObservation if s is not None])
+
+    # Create the batch tensors - for the observations, actions and rewards
+    # This is needed to calculate the expected observation action values
+    # to then calculate the loss
+    observationBatch = torch.cat(batchedSamples.observation)
+    actionBatch = torch.cat(batchedSamples.action)
+    rewardBatch = torch.cat(batchedSamples.reward)
+
+    # Run these through the network
+    ActionValues = policyModel(observationBatch).gather(1, actionBatch)
+    nextActionValues = torch.zeros(params.batchSize, device=params.device)
+    with torch.no_grad():
+        nextActionValues[nonFinalBoolMask] = targetModel(nonFinalNextObservations).max(1).values
+  
+    expectedActionValues = rewardBatch + (nextActionValues * params.gamma)
+
+    criterion = torch.nn.SmoothL1Loss()
+    loss = criterion(ActionValues, expectedActionValues.unsqueeze(1))
+
+ 
+    params.optimiser.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(policyModel.parameters(),1)
+    params.optimiser.step()
+    
