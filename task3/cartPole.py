8497d5be59c4db44b770ab5ec7933361d9bc6335
Olliemyers4 <olliemyers4@gmail.com>
2024-08-26 13:07:54 +0100

    Added plotting and model saving

--- task3/cartPole.py
+++ task3/cartPole.py
@@ -2,16 +2,27 @@ import dqn
 import gymnasium as gym
 import torch
 import math
+import matplotlib.pyplot as plt
 
 def epsilonDecay(start,end,decayRate,steps):
     return end + (start - end) * math.exp(-1. * steps / decayRate)
+
+def plotScores(scores):
+    plt.figure(1)
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(scores)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
 
 env = gym.make('CartPole-v1',render_mode = "human")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-policyNet = dqn.DQN(env.observation_space.shape[0], 32, env.action_space.n).to(device)
-targetNet = dqn.DQN(env.observation_space.shape[0], 32, env.action_space.n).to(device)
+policyNet = dqn.DQN(env.observation_space.shape[0], 64, env.action_space.n).to(device)
+targetNet = dqn.DQN(env.observation_space.shape[0], 64, env.action_space.n).to(device)
 targetNet.load_state_dict(policyNet.state_dict())
 
 memory = dqn.ReplayMemory(1000)
@@ -28,12 +39,16 @@ epsStart = 1
 epsEnd = 0.01
 epsDecay = 1000
 
+scores = []
+
+plt.ion()
 
 for _ in range(episodes):
     observation, info = env.reset()
     observation = torch.tensor([observation], device=params.device, dtype=torch.float)
-
+    thisEpisodes = 0
     while True:
+        
         #while we can continue
         epsilonThreshold = epsilonDecay(epsStart,epsEnd,epsDecay,steps)
         action = dqn.selectAction(observation=observation,epsilonThreshold=epsilonThreshold,env=env,model=policyNet,params=params)
@@ -41,6 +56,7 @@ for _ in range(episodes):
         actionOutcome, reward, terminated, truncated, info = env.step(action.item())
 
         reward = torch.tensor([reward], device=params.device)
+        thisEpisodes += 1
         
         if (terminated):
             nextObservation = None
@@ -60,6 +76,15 @@ for _ in range(episodes):
             targetState[key] = policyState[key] * params.tau + targetState[key] * (1.0 - params.tau)
 
         if (terminated or truncated):
+            scores.append(thisEpisodes)
             break
+        
+        plotScores(scores)
 
 env.close()
+plt.ioff()
+plt.show()
+
+torch.save(targetNet.state_dict(), "cartPole.pth")
+print("Model saved")
+print("Best score: ", max(scores))
